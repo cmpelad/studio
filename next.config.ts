bab3e0397f4220cd86cf8b2debd4993e8c0475b5
גן ישראל אלעד - התשפ'ה - 5785 <gielad5785@gmail.com>
2025-05-21 19:18:56 +0000

    תגדיר שיתאים לפריסה ב cloudflare.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Add this line for static HTML export
   typescript: {
     ignoreBuildErrors: true,
   },
